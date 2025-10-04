7fc5ce8f2db93b0d2f9432e3db36dda98cbf8ca9
Maximilian Rosenmüller <maximilian.rosenmueller@trustcaptcha.com>
2025-10-04 17:27:22 +0200

    Fix loading problem

--- registration.php
+++ registration.php
@@ -1,6 +1,20 @@
 <?php
-use Magento\Framework\Component\ComponentRegistrar;
 
+// PSR-4 Fallback
+spl_autoload_register(static function ($class) {
+    $prefix = 'TrustComponent\\TrustCaptchaMagento2\\';
+    $len = strlen($prefix);
+    if (strncmp($class, $prefix, $len) !== 0) {
+        return;
+    }
+    $rel  = substr($class, $len);
+    $file = __DIR__ . '/' . str_replace('\\', '/', $rel) . '.php';
+    if (is_file($file)) {
+        require $file;
+    }
+}, true, true);
+
+use Magento\Framework\Component\ComponentRegistrar;
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'TrustComponent_TrustCaptchaMagento2',
