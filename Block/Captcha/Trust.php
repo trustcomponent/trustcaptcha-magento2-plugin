37fb912f262c008eae83f14ab65e3fa371bd287a
Maximilian Rosenmüller <maximilian.rosenmueller@trustcaptcha.com>
2025-10-04 16:09:24 +0200

    Rename module to TrustCaptchaMagento2

--- Block/Captcha/Trust.php
+++ Block/Captcha/Trust.php
@@ -6,7 +6,7 @@ use TrustComponent\TrustCaptchaMagento2\Model\Config;
 
 class Trust extends MagentoDefaultCaptcha
 {
-    protected $_template = 'TrustComponent_TrustCaptcha::captcha/trustcaptcha.phtml';
+    protected $_template = 'TrustComponent_TrustCaptchaMagento2::captcha/trustcaptcha.phtml';
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
--- registration.php
+++ registration.php
@@ -3,6 +3,6 @@ use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
-    'TrustComponent_TrustCaptcha',
+    'TrustComponent_TrustCaptchaMagento2',
     __DIR__
 );
